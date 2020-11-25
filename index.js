a062eab6a7520e801221524b690380304751f7ad
ArtemTishenko <tema.tishenko1993@yandex.ru>
2020-11-26 01:19:59 +0300

    refactor: удалить лишние коменатрии в index.js feat: добавить состояние :active для element__like и убрать padding feat: добавить атрубут arial-label для кнопок fix: исправить путь к logo в header refactor: добавить описание в README.md refactor: оптимизировать картинки refactor: добавить карточки согласно макету в figma"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//******Открытие и закрытие popup окна
+//******Открытие и закрытие popup окна*****************************//
 const profileButtonInfoEddit = document.querySelector('.profile__button-info-eddit');
 const popup = document.querySelector('.popup');
 const popupButtonClose = document.querySelector('.popup__button-close');
@@ -7,56 +7,47 @@ profileButtonInfoEddit.addEventListener('click', openPopup );
 popupButtonClose.addEventListener('click', closePopup );
 
 function openPopup() {
-
   popup.classList.add('popup_visible');
 }
 
 function closePopup() {
   popup.classList.remove('popup_visible');
 }
-//**********************************************************************
+//**********************************************************************/
 
 
 //******************************************************************* */
-// Находим форму в DOM
-const popupContainer = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+  // Находим форму в DOM
+ const popupContainer = document.querySelector('.popup__container');
 
- let nameInput = popupContainer.querySelector('.popup__field_name');
- let jobInput =popupContainer.querySelector('.popup__field_job');
+ const nameInput = popupContainer.querySelector('.popup__field_name');
+ const jobInput =popupContainer.querySelector('.popup__field_job');
 
- let profileInfoNameNode = document.querySelector('.profile__info-name');
- let profileInfoJobNode = document.querySelector('.profile__info-job');
-
-
+ const profileInfoNameNode = document.querySelector('.profile__info-name');
+ const profileInfoJobNode = document.querySelector('.profile__info-job');
+  //Записывание в форму текущие знчения блока profile
  nameInput.value = profileInfoNameNode.textContent;
  jobInput.value = profileInfoJobNode.textContent;
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
  function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                         // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки
 
-    // Находим поля формы в DOM
-    let nameInput = popupContainer.querySelector('.popup__field_name');// Воспользуйтесь инструментом .querySelector()
-    let jobInput =popupContainer.querySelector('.popup__field_job'); // Воспользуйтесь инструментом .querySelector()
+  // Находим поля формы в DOM
+  let nameInput = popupContainer.querySelector('.popup__field_name');
+  let jobInput =popupContainer.querySelector('.popup__field_job');
 
-    // Получите значение полей из свойства value
+  // Выбираем элементы, куда должны быть вставлены значения полей
+  let profileInfoNameNode = document.querySelector('.profile__info-name');
+  let profileInfoJobNode = document.querySelector('.profile__info-job');
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileInfoNameNode = document.querySelector('.profile__info-name');
-    let profileInfoJobNode = document.querySelector('.profile__info-job');
+  // Вставляем новые значения с помощью textContent
+  profileInfoNameNode.textContent=nameInput.value;
+  profileInfoJobNode.textContent=jobInput.value;
 
-    // Вставьте новые значения с помощью textContent
-    profileInfoNameNode.textContent=nameInput.value;
-    profileInfoJobNode.textContent=jobInput.value;
-    closePopup()
+  closePopup()
  }
 
- // Прикрепляем обработчик к форме:
- // он будет следить за событием “submit” - «отправка»
-const popupButtonSubmitClick = document.querySelector('.popup__button-submit');
+ const popupButtonSubmitClick = document.querySelector('.popup__button-submit');
 
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
  popupButtonSubmitClick.addEventListener('click', formSubmitHandler);
