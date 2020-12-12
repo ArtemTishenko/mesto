c9eee182556006d923d3cfb958ec3da84b9721e5
ArtemTishenko <tema.tishenko1993@yandex.ru>
2020-12-12 03:24:55 +0300

    refactor: переменные let заданы как const fix: создать две унифицированные функции открытия и закрытия окна fix: создать новый элемент карточки с помощью функции composeCard

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,6 @@ const popupContainerEdditForm = document.querySelector('.popup__container_type_e
 const nameInput = popupContainerEdditForm.querySelector('.popup__field_type_name');
 const jobInput =popupContainerEdditForm.querySelector('.popup__field_type_job');
 
-
 const popupContainerCard = document.querySelector('.popup__container_type_card');
 const popupContainerCardName = popupContainerCard.querySelector('.popup__field_type_card-name');
 const popupContainerCardLink = popupContainerCard.querySelector('.popup__field_type_card-link');
@@ -25,6 +24,9 @@ const profileInfoJobNode = document.querySelector('.profile__info-job');
 
 const listContainerElement = document.querySelector('.elements');
 const templateCard = document.querySelector('.template');
+
+const popupPicture = popupImg.querySelector('.popup__picture');
+const popupPictureCaption = popupImg.querySelector('.popup__picture-caption');
 
 const initialCards = [
   {
@@ -54,29 +56,27 @@ const initialCards = [
 ];
 
 //*****************************Открытие и закрытие popup окон*****************************//
+function openModal(dompopup){
+  dompopup.classList.add('popup_visible');
+}
+function closeModal(dompopup){
+  dompopup.classList.remove('popup_visible');
+}
 function openPopupEdditForm(){
-  popupEditForm.classList.add('popup_visible');
-
   nameInput.value = profileInfoNameNode.textContent; //подтяжка с profile-info в форму
   jobInput.value = profileInfoJobNode.textContent;
-}
-function closePopupEdditForm(){
-  popupEditForm.classList.remove('popup_visible');
+
+  openModal(popupEditForm);
 }
 function openPopupCard(){
+
   popupContainerCardName.value = "";
   popupContainerCardLink.value = "";
 
-  popupCard.classList.add('popup_visible');
-}
-function closePopupCard(){
-  popupCard.classList.remove('popup_visible');
 
+  openModal(popupCard);
 }
 function openPopupImg(event){
-  const popupPicture = popupImg.querySelector('.popup__picture');
-  const popupPictureCaption = popupImg.querySelector('.popup__picture-caption');
-
   const eventTargetSrc = event.target.getAttribute('src');
   const eventTargetAlt = event.target.getAttribute('alt');
   const eventTargetCaption = event.target.parentElement.querySelector('.element__caption').innerHTML ;
@@ -85,42 +85,32 @@ function openPopupImg(event){
   popupPicture.setAttribute('alt', eventTargetAlt);
   popupPictureCaption.textContent = eventTargetCaption;
 
-  popupImg.classList.add('popup_visible');
-}
-function closePopupImg(){
-  popupImg.classList.remove('popup_visible');
+  openModal(popupImg);
 }
 //*************************************************************************************** //
-
 function formSubmitHandler (event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки
 
   profileInfoNameNode.textContent=nameInput.value;// Вставляем новые значения с помощью textContent
   profileInfoJobNode.textContent=jobInput.value;
 
-  closePopupEdditForm();
+  closeModal(popupEditForm);
 }
 function addNewCard (event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки
-  let newCardElement = [];
 
-  let newCardName = popupContainerCardName.value;// Вставляем название карточки
-  let newCardLink = popupContainerCardLink.value;// Вставляем ссылку на картинку для карточки
+  const newCardName = popupContainerCardName.value;// Вставляем название карточки
+  const newCardLink = popupContainerCardLink.value;// Вставляем ссылку на картинку для карточки
 
-  newCardElement.push({name:`${newCardName}`, link: `${newCardLink}`});
+  const newCarItem = composeCard({name:newCardName,link: newCardLink})
+  listContainerElement.prepend(newCarItem);
 
-  let newCarItem = newCardElement.map(composeCard);
-  listContainerElement.prepend(...newCarItem);
-
-  composeCard(newCardElement);
-  closePopupCard();
+  closeModal(popupCard);
 }
-
 function renderCard(){ // функция добавления карточки из массива
   const cardItems = initialCards.map(composeCard);
   listContainerElement.append(...cardItems);
 }
-
 function composeCard(item){ // Функция клонирования template, добавления картинки и имени карточки
   const newCard = templateCard.content.cloneNode(true);
   const elementCaption = newCard.querySelector('.element__caption');
@@ -128,42 +118,31 @@ function composeCard(item){ // Функция клонирования template,
   const elementDelete = newCard.querySelector('.element__delete'); //* delete button
   const elementLike = newCard.querySelector('.element__like');//* like button
 
-
   elementCaption.textContent = item.name;
   elementImg.setAttribute('src', `${item.link}`);
   elementImg.setAttribute('alt',`${item.name}`);
-
-  const elementImgSrc = elementImg.getAttribute('src');
-  const elementImgAlt = elementImg.getAttribute('alt');
-  //console.log('elementImgSrc ' + elementImgSrc);
-  //console.log('elementImgAlt ' + elementImgAlt);
 
   elementDelete.addEventListener('click',clickRemoveButton); //* delete button
   elementLike.addEventListener('click', clickLikeButton);//* like button
 
   elementImg.addEventListener('click',openPopupImg);
-  popupButtonCloseImg.addEventListener('click', closePopupImg);
-
 
   return newCard;
 }
-
-
 function clickRemoveButton(event){ //Функция удления карточки
   const eventTarget= event.target.closest('.element');
   eventTarget.remove();
 }
-
 function clickLikeButton(event){ //Функция установки like
   const eventTarget = event.target;
   eventTarget.classList.toggle('element__like_active');
 }
 
-profileButtonInfoEddit.addEventListener('click', openPopupEdditForm );
+profileButtonInfoEddit.addEventListener('click', openPopupEdditForm);
 profileButtonAdd.addEventListener('click', openPopupCard);
-popupButtonClose.addEventListener('click', closePopupEdditForm );
-popupButtonCloseCard.addEventListener('click', closePopupCard);
-
+popupButtonClose.addEventListener('click', function(){closeModal(popupEditForm)});
+popupButtonCloseCard.addEventListener('click', function(){closeModal(popupCard)});
+popupButtonCloseImg.addEventListener('click', function(){closeModal(popupImg)});
 // Прикрепляем обработчики к форме: он будет следить за событием “submit” - «отправка»
 popupContainerEdditForm.addEventListener('submit', formSubmitHandler);
 popupContainerCard.addEventListener('submit', addNewCard);
