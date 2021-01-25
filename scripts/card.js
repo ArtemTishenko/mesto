50be7a2275865a43b73755c19b7791c195ec3b9b
ArtemTishenko <tema.tishenko1993@yandex.ru>
2021-01-25 18:08:47 +0300

    feat: создать класс Card и formValidator

---
+++ scripts/card.js
@@ -0,0 +1,48 @@
+import {openPopupImg} from './index.js';
+export class Card {
+  constructor(item, cardSelector) {
+    this._item = item;
+    this._name = item.name;
+    this._link = item.link;
+    this._cardSelector = cardSelector;
+  }
+  _getTemplate() {// возвращает шаблон разметки карточки
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".element")
+      .cloneNode(true);
+    return cardElement;
+  }
+  _clickLikeButton() {
+    this._element.querySelector(".element__like").classList.toggle("element__like_active");
+  }
+  _clickRemoveButton() {
+    this._element.remove();
+  }
+  _setEventListeners() {// вызов слушателей: лайк, удаление, popupImg ->(open modal -> добавление слушателя клика по overlay)
+    const elementDelete = this._element.querySelector(".element__delete");
+    const elementLike = this._element.querySelector(".element__like");
+    const elementImg = this._element.querySelector(".element__img");
+
+    elementDelete.addEventListener("click", () => {
+      this._clickRemoveButton();
+    });
+    elementLike.addEventListener("click", () => {
+      this._clickLikeButton();
+    });
+    elementImg.addEventListener("click", openPopupImg);
+
+  }
+  generateCard() {
+    this._element = this._getTemplate();// клонирование шаблона карточки
+    this._element.querySelector(".element__caption").textContent = this._name;
+    this._element.querySelector(".element__img").setAttribute("src", `${this._link}`);
+    this._element.querySelector(".element__img").setAttribute("alt", `${this._name}`);
+
+    this._setEventListeners();
+
+
+    return this._element;
+  }
+  render
+}
