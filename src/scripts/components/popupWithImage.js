3d790aa36e13edc79fb560862197cd6954bb58b5
ArtemTishenko <tema.tishenko1993@yandex.ru>
2021-02-08 22:04:55 +0300

    элементы popupCardImage и captionImage ищуться в конструкторе

--- src/scripts/components/popupWithImage.js
+++ src/scripts/components/popupWithImage.js
@@ -2,15 +2,14 @@ import { Popup } from "./popup.js";
 export class PopupWithImage extends Popup {
   constructor(popupElement) {
     super(popupElement);
+    this._popupCardImage = this._popupElement.querySelector(".popup__picture");
+    this._captionImage = this._popupElement.querySelector(".popup__picture-caption");
   }
   open(src, alt) {
     super.open();
-    const elementImg = this._popupElement.querySelector(".popup__picture");
-    const elementCaption = this._popupElement.querySelector(
-      ".popup__picture-caption"
-    );
-    elementImg.src = src;
-    elementImg.alt = alt;
-    elementCaption.textContent = alt;
+    
+    this._popupCardImage.src = src;
+    this._captionImage.alt = alt;
+    this._captionImage.textContent = alt;
   }
 }
